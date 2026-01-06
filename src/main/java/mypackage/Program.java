6892f7231722356636579c6283de9c1f97086c3b
Pratiksha <pratikshanimbalkar1210@gmail.com>
2026-01-06 16:20:24 +0530

    Initial commit

---
+++ src/main/java/mypackage/Program.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub'
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Add="+c);
+
+	}
+
+}
